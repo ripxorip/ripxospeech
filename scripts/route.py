d0ecd0c42b70e5d4af672209637369bb44a18cba
Philip K. Gisslow <ripxorip@gmail.com>
2023-11-10 14:29:48 +0100

    feat: Pulse/pipewire routing for Windows VM (neet to tune latency somewhere)

--- scripts/route.py
+++ scripts/route.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
-import os
-import sys
 import subprocess
 import argparse
-import time
 
 IP_ADDR = {
-    "lab": "",
+    "lab": "100.100.250.30",
     "engine_talon": "100.97.216.58",
     "engine_win11_swe": "",
     "work": "100.101.164.159",
@@ -25,6 +22,7 @@ LINUX_USER="ripxorip"
 TMUX_SESSION_NAME="ripxospeech"
 
 def run_command_over_ssh(command, server):
+    print("Running command: {} on {}".format(command, server))
     ssh = subprocess.Popen(["ssh", "{}@{}".format(LINUX_USER, server), command],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     result = ssh.stdout.readlines()
     stderr = ssh.stderr.readlines()
@@ -79,9 +77,32 @@ def setup_talon(server, client):
     run_command_over_ssh("echo -e 'server_ip={}\nserver_port={}' > ~/dev/x11_keysender/client.txt".format(IP_ADDR[client], VOICE_BOX_CLIENT_PORT), IP_ADDR[server])
     route_gst_client(client, server)
 
+def setup_win11_swe(client):
+    server = "lab"
+    start_gst_server(server)
+    route_gst_client(client, server)
+
+    # FIXME Not sure that all of these are needed
+    run_command_over_ssh("pw-link -d alsa_output.pci-0000_08_00.1.hdmi-stereo-extra1:monitor_FL Win11SweGenuine:input_FL", IP_ADDR[server])
+    run_command_over_ssh("pw-link -d alsa_output.pci-0000_08_00.1.hdmi-stereo-extra1:monitor_FR Win11SweGenuine:input_FR", IP_ADDR[server])
+
+    run_command_over_ssh("pw-link -d Win11SweGenuine:output_FL alsa_output.pci-0000_08_00.1.hdmi-stereo-extra1:playback_FL", IP_ADDR[server])
+    run_command_over_ssh("pw-link -d Win11SweGenuine:output_FR alsa_output.pci-0000_08_00.1.hdmi-stereo-extra1:playback_FR", IP_ADDR[server])
+
+    run_command_over_ssh("pw-link -d gst-launch-1.0:output_FL alsa_output.pci-0000_08_00.1.hdmi-stereo-extra1:playback_FL", IP_ADDR[server])
+    run_command_over_ssh("pw-link -d gst-launch-1.0:output_FR alsa_output.pci-0000_08_00.1.hdmi-stereo-extra1:playback_FR", IP_ADDR[server])
+
+    run_command_over_ssh("pw-link -d Win11SweGenuine:input_FR alsa_output.pci-0000_08_00.1.hdmi-stereo-extra1:monitor_FR", IP_ADDR[server])
+    run_command_over_ssh("pw-link -d Win11SweGenuine:input_FL alsa_output.pci-0000_08_00.1.hdmi-stereo-extra1:monitor_FL", IP_ADDR[server])
+
+    # This one actually routes the audio to the Windows VM
+    run_command_over_ssh("pw-link gst-launch-1.0:output_FL Win11SweGenuine:input_FL", IP_ADDR[server])
+
+    print("hint: To manually verify that the routing is correct: run the following command: pw-link -I -l")
+
 def main():
     # Parse the arguments
-    parser = argparse.ArgumentParser(description="Tool to use for routing my voice to different speech  recognition servers")
+    parser = argparse.ArgumentParser(description="Tool to use for routing my voice to different speech recognition servers")
     # Add the arguments
     parser.add_argument("-c", "--client", help="The client to use", choices=["work", "station"])
     parser.add_argument("-e", "--engine", help="the speech engine to use", choices=["talon", "win11_swe"])
@@ -97,6 +118,8 @@ def main():
         exit(1)
     elif args.engine == "talon":
         setup_talon("engine_talon", args.client)
+    elif args.engine == "win11_swe":
+        setup_win11_swe(args.client)
 
 if __name__ == "__main__":
     main()
