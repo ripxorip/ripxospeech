681611adc41231e012d615fa2a5acfa65ba0f4ca
Philip K. Gisslow <ripxorip@gmail.com>
2023-12-02 09:29:10 +0100

    chore: more refectoring

--- ripxospeech.py
+++ ripxospeech.py
@@ -8,6 +8,36 @@ from utils.constants import *
 from utils.tmux import *
 from utils.keyboard_server_api import *
 
+def route(args):
+    tmux_run(
+        "gst-launch-1.0 -v pulsesrc buffer-time=100000 latency-time=10000 ! opusenc ! "
+        "rtpopuspay ! queue max-size-buffers=200 max-size-time=20000000 max-size-bytes=2000 ! "
+        "multiudpsink clients={}:{},{}:{} buffer-size=200".format(
+            IP_ADDR["engine_talon"], GST_SOUND_PORT, IP_ADDR["engine_win11_swe"], GST_SOUND_PORT
+        ), 
+        args.client
+    )
+
+def kill():
+    tmux_kill()
+
+def serve():
+    from keyboard_server.serve import KeyboardServer
+    server = KeyboardServer()
+    server.run()
+
+def start_dictation(args):
+    if args.engine == "talon_dictation":
+        send_command_to_keyboard_server("start_talon_dictation")
+    elif args.engine == "talon_command":
+        send_command_to_keyboard_server("start_talon_command")
+    elif args.engine == "win11_swe":
+        send_command_to_keyboard_server("start_win11_swe")
+    elif args.engine == "gdocs":
+        send_command_to_keyboard_server("start_gdocs")
+
+def stop_dictation():
+    send_command_to_keyboard_server("stop")
 
 def main():
     # Parse the arguments
@@ -20,39 +50,15 @@ def main():
     args = parser.parse_args()
 
     if args.action == "route":
-        tmux_run(
-            "gst-launch-1.0 -v pulsesrc buffer-time=100000 latency-time=10000 ! opusenc ! "
-            "rtpopuspay ! queue max-size-buffers=200 max-size-time=20000000 max-size-bytes=2000 ! "
-            "multiudpsink clients={}:{},{}:{} buffer-size=200".format(
-                IP_ADDR["engine_talon"], GST_SOUND_PORT, IP_ADDR["engine_win11_swe"], GST_SOUND_PORT
-            ), 
-            args.client
-        )
-
+        route(args)
     elif args.action == "kill":
-        tmux_kill()
-
+        kill()
     elif args.action == "serve":
-        from keyboard_server.serve import KeyboardServer
-        server = KeyboardServer()
-        server.run()
-
+        serve()
     elif args.action == "start_dictation":
-
-        if args.engine == "talon_dictation":
-            send_command_to_keyboard_server("start_talon_dictation")
-
-        elif args.engine == "talon_command":
-            send_command_to_keyboard_server("start_talon_command")
-
-        elif args.engine == "win11_swe":
-            send_command_to_keyboard_server("start_win11_swe")
-
-        elif args.engine == "gdocs":
-            send_command_to_keyboard_server("start_gdocs")
-
+        start_dictation(args)
     elif args.action == "stop_dictation":
-        send_command_to_keyboard_server("stop")
+        stop_dictation()
 
 if __name__ == "__main__":
     main()
