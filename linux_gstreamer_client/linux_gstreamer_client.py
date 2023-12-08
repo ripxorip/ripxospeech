c0283f7de190aa48703aefe7707b741cee7a2e20
Philip K. Gisslow <ripxorip@gmail.com>
2023-12-08 07:11:00 +0100

    feat: VSCode task for server and python client (needs to be rewritten)

--- linux_gstreamer_client/linux_gstreamer_client.py
+++ linux_gstreamer_client/linux_gstreamer_client.py
@@ -5,25 +5,14 @@ import sys
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GLib
 
-IP_ADDR = {
-    "lab": "100.100.250.30",
-    "engine_talon": "100.97.216.58",
-    "engine_win11_swe": "100.106.115.19",
-    "work": "100.101.164.159",
-    "station": "100.121.51.47",
-}
-
-CLIENTS = ["work", "station"]
-SERVERS = ["lab", "engine_talon", "engine_win11_swe"]
-
-VOICE_BOX_CLIENT_PORT="5000"
-
+SOUND_SERVER_IP = "100.100.250.30"
 GST_SOUND_PORT="5137"
+
 # Initialize GStreamer
 Gst.init(None)
 
 # Build the pipeline
-pipeline = Gst.parse_launch("pulsesrc buffer-time=100000 latency-time=10000 ! opusenc ! rtpopuspay ! queue max-size-buffers=200 max-size-time=20000000 max-size-bytes=2000 ! multiudpsink clients={}:{},{}:{} buffer-size=200".format(IP_ADDR["engine_talon"], GST_SOUND_PORT, IP_ADDR["engine_win11_swe"], GST_SOUND_PORT))
+pipeline = Gst.parse_launch("pulsesrc buffer-time=100000 latency-time=10000 ! opusenc frame-size=20 ! rtpopuspay ! queue max-size-buffers=200 max-size-time=20000000 max-size-bytes=2000 ! udpsink host={} port={} buffer-size=200 buffer-size=200".format(SOUND_SERVER_IP, GST_SOUND_PORT))
 
 # Start playing
 pipeline.set_state(Gst.State.PLAYING)
