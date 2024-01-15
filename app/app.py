7eb8dc844864917240e440cb7c23bf51766b6f51
Philip K. Gisslow <ripxorip@gmail.com>
2024-01-15 19:51:46 +0100

    feat: Adds support for starting/stopping the client stream. The same shall be done for the audio server (Rpi) use setsid etc when wrapping to a socket server.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
+import subprocess
+import os
+import signal
+
 class App:
     def __init__(self):
-        print("App init")
+        self.stream_process = None
 
     def button_clicked(self, button):
         print("button clicked")
@@ -11,9 +15,13 @@ class App:
     def start_audio_stream(self):
         # Shall start the local gstreamer pipeline
         # and send a message to the gstreamer server to start listening
-        print("start_audio_stream")
+        # Set os.setsid() to make it run in a new process group,
+        # that way we can kill the entire process group when we want to stop the stream
+        self.stream_process = subprocess.Popen(["linux_gstreamer_client/stream.sh"], preexec_fn=os.setsid)
 
     def stop_audio_stream(self):
         # Shall stop the local gstreamer pipeline
         # and send a message to the gstreamer server to stop listening
-        print("stop_audio_stream")
+        if self.stream_process is not None:
+            os.killpg(os.getpgid(self.stream_process.pid), signal.SIGINT)
+            self.stream_process = None
