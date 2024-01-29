ca15e0004da17c4b42f4393f08ad26d543dad2a0
Philip K. Gisslow <ripxorip@gmail.com>
2024-01-29 17:46:26 +0100

    fix: Adds missing gstreamer

--- ripxospeech_gtk/default.nix
+++ ripxospeech_gtk/default.nix
@@ -3,6 +3,7 @@
 , gtk4
 , libadwaita
 , wrapGAppsHook4
+, gst_all_1
 , gobject-introspection
 , virtual_keyboard
 }:
@@ -11,7 +12,7 @@ buildPythonApplication {
   pname = "Ripxospeech gtk4 GUI";
   version = "1.0";
 
-  nativeBuildInputs = [ gobject-introspection wrapGAppsHook4 libadwaita gtk4 virtual_keyboard ];
+  nativeBuildInputs = [ gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly gst_all_1.gst-libav gobject-introspection wrapGAppsHook4 libadwaita gtk4 virtual_keyboard ];
 
   propagatedBuildInputs = [ pygobject3 pyserial libadwaita gtk4 pytesseract virtual_keyboard ];
 
