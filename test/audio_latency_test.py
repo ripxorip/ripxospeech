222af1a37515de180f488319ecb6a53878c29e35
Philip K. Gisslow <ripxorip@gmail.com>
2024-03-11 19:39:51 +0100

    feat: Now saves the recording for future reference

--- test/audio_latency_test.py
+++ test/audio_latency_test.py
@@ -4,6 +4,7 @@ import sounddevice as sd
 import socket
 import threading
 import time
+import wave
 import sys
 import os
 
@@ -19,6 +20,7 @@ class Server:
         self.has_peak = False
         self.ip_thread_handle = threading.Thread(target=self.ip_thread)
         self.ip_thread_handle.start()
+        self.current_recording = []
         with sd.InputStream(callback=self.audio_callback, blocksize=buffer_size, samplerate=sample_rate, channels=1) as stream:
             print("Input latency:", stream.latency*1000, "ms")
             sd.sleep(10000000)
@@ -26,9 +28,18 @@ class Server:
     def audio_callback(self, indata, frames, time, status):
         for s in indata:
             sample = s[0]
+            self.current_recording.append(sample)
             if abs(sample) > threshold and not self.has_peak:
                 self.has_peak = True
                 print("Threshold exceeded")
+                # Convert the floating-point numbers to 16-bit integers
+                int_samples = (np.array(self.current_recording) * 32767).astype(np.int16)
+                # Save the current recording as a wav file using wave
+                with wave.open("/tmp/rec.wav", 'w') as wf:
+                    wf.setnchannels(1)
+                    wf.setsampwidth(2)  # Set sample width to 2 bytes for 16-bit audio
+                    wf.setframerate(sample_rate)
+                    wf.writeframes(int_samples.tobytes())
                 # Send threshold exceeded message to client
                 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 server_address = (self.ip_address, 7822)
@@ -43,6 +54,7 @@ class Server:
             if data:
                 self.ip_address = address[0]
                 self.has_peak = False
+                self.current_recording = []
 
 class Client:
     def __init__(self, server_ip):
@@ -79,7 +91,9 @@ class Client:
                 # Save the current time
                 self.my_peak_time = time.time()
 
-# FIXME Also save some audio and send back to verify that the quality was ok somehow!
+# Get the files by:
+# scp ripxorip@voiceboxlinux:/tmp/rec.wav /tmp/rec.wav && vlc /tmp/rec.wav
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("mode", choices=['server', 'client'], help="run as server or client")
