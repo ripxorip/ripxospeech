584f2bae4d31b052619644a38826d02ac3344b70
Philip K. Gisslow <ripxorip@gmail.com>
2024-04-16 11:27:45 +0200

    feat: XML format for device detection

--- utils/identify_usb_soundcards.py
+++ utils/identify_usb_soundcards.py
@@ -2,6 +2,7 @@ import subprocess
 import json
 import time
 import wave
+import re
 
 class SoundCardIdentifier:
     def __init__(self):
@@ -39,21 +40,65 @@ class SoundCardIdentifier:
         # Find the master with the highest slave max
         max_master = max(max_vals, key=lambda x: sum(x[1]))[0]
 
+        base_str = """
+        <hostdev mode='subsystem' type='usb' managed='no'>
+            <source>
+                <address bus='{bus}' device='{device}'/>
+            </source>
+        </hostdev>
+        """
+
         print("\n\n\n** Master device: **")
         print(max_master)
         print("** Slave devices: **")
+        self.slaves = []
         for d in self.devices:
             if d != max_master:
                 print(d)
+                self.slaves.append(d)
 
-        # FIXME it shall generate the kvm commands to passthrough the masters/slaves to KVM instead somehow?
-        with open('/tmp/ripxospeech_devices', 'w') as f:
-            f.write("** Master device: **\n")
-            f.write(str(max_master) + '\n')
-            f.write("** Slave devices: **\n")
-            for d in self.devices:
-                if d != max_master:
-                    f.write(str(d) + '\n')
+        with open('/tmp/ripxospeech_slave_one.xml', 'w') as f:
+            f.write(base_str.format(bus=slaves[0]['usb']['bus'], device=slaves[0]['usb']['dev']))
+
+        with open('/tmp/ripxospeech_slave_two.xml', 'w') as f:
+            f.write(base_str.format(bus=slaves[1]['usb']['bus'], device=slaves[1]['usb']['dev']))
+
+        with open('/tmp/ripxospeech_master.xml', 'w') as f:
+            f.write(base_str.format(bus=max_master['usb']['bus'], device=max_master['usb']['dev']))
+
+    def correlate_bus_port_to_device(self, bus, port):
+        bus = int(bus)
+        print(f"Bus: {bus}, Port: {port}")
+        # Get the result of lsusb -t
+        res = subprocess.run(["lsusb", "-t"], stdout=subprocess.PIPE)
+        res = res.stdout.decode('utf-8')
+        busFound = False
+        portPartOnefound = False
+        portPartTwoFound = False
+        portPartOne = port.split(".")[0]
+        portPartTwo = port.split(".")[1]
+        for l in res.split("\n"):
+            if not busFound:
+                if l.startswith("/:  Bus "):
+                    pattern = r"/:  Bus (\d+)"
+                    busNum = int(re.search(pattern, l).group(1))
+                    if busNum == bus:
+                        busFound = True
+            elif not portPartOnefound:
+                if l.startswith("    |__ Port "):
+                    pattern = r"    \|__ Port (\d+)"
+                    portNum = int(re.search(pattern, l).group(1))
+                    if portNum == int(portPartOne):
+                        portPartOnefound = True
+            elif not portPartTwoFound:
+                if l.startswith("        |__ Port "):
+                    pattern = r"        \|__ Port (\d+)"
+                    portNum = int(re.search(pattern, l).group(1))
+                    if portNum == int(portPartTwo):
+                        portPartTwoFound = True
+                        pattern = r"        \|__ Port \d+: Dev (\d+)"
+                        devNum = int(re.search(pattern, l).group(1))
+                        return {"bus": bus, "dev": devNum}
 
     def enumerate(self):
         # Parse output of pw-dump as json
@@ -70,7 +115,8 @@ class SoundCardIdentifier:
                 su = u.split("-")
                 bus = su[0]
                 port =su[1]
-                d = {'alsa': i["info"]["props"]["api.alsa.path"], 'usb': {'bus': bus, 'port': port}}
+                usb_bus_dev = self.correlate_bus_port_to_device(bus, port)
+                d = {'alsa': i["info"]["props"]["api.alsa.path"], 'usb': usb_bus_dev}
                 self.devices.append(d)
 
 def main():
