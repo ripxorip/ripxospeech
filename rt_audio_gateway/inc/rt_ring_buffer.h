f4ed127ae961c0ea5137c7a476424195f5faddce
Philip K. Gisslow <ripxorip@gmail.com>
2024-06-14 17:40:26 +0200

    fix: Naming

--- rt_audio_gateway/inc/rt_ring_buffer.h
+++ rt_audio_gateway/inc/rt_ring_buffer.h
@@ -1,5 +1,5 @@
-#ifndef RING_BUFFER
-#define RING_BUFFER
+#ifndef RT_RING_BUFFER
+#define RT_RING_BUFFER
 
 #include <stdlib.h>
 #include <string.h>
@@ -19,4 +19,4 @@ int ring_buffer_push(ring_buffer_t *ring_buffer, rt_stream_packet_t *data);
 int ring_buffer_pop(ring_buffer_t *ring_buffer, rt_stream_packet_t *data);
 void ring_buffer_clear(ring_buffer_t *ring_buffer);
 
-#endif /* RING_BUFFER */
+#endif /* RT_RING_BUFFER */
